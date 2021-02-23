4bd70a90eded2c2bb2734a0c451624ed0bf7a806
Tutospin <79525091+Tutospin@users.noreply.github.com>
2021-02-23 15:36:23 +0100

    Add files via upload

---
+++ tutospin/src/js/main.js
@@ -0,0 +1,34 @@
+$('#send_email').click(function(e){
+    e.preventDefault();
+    var data = {
+        email: $('#email').val(),
+        name: $('#name').val(),
+        objet: $('#firstname').val(),
+        message: $('#message').val()
+    };
+    $.ajax({
+        url: "mail.php",
+        type: 'POST',
+        data: data,
+        success: function(data) {
+            $('#js_alert_success').css({'display' : 'block'});
+            setTimeout(function(){
+                $('#js_alert_success').css({'display' : 'none'});
+                $('#email').val("");
+                $('#name').val("");
+                $('#object').val("");
+                $('#message').val("")
+            }, 3000);
+        },
+        error: function(data) {
+            $('#js_alert_danger').css({'display' : 'block'});
+            setTimeout(function(){
+                $('#js_alert_danger').css({'display' : 'none'});
+                $('#email').val("");
+                $('#name').val("");
+                $('#object').val("");
+                $('#message').val("")
+            }, 3000);
+        }
+    });
+});
